30bfd5401eff4b2f8c0c3e2607f436ff7a48ca40
Soh <columbine.k@gmail.com>
2026-08-10 22:24:14 +0800

    feat: switch to Aurora theme and tune firmware defaults
    - Replace luci-theme-argon with luci-theme-aurora in diy scripts and configs
    - Drop 18.06-era LuCI patches from diy scripts
    - Remove X86_64 mini workflow and its config/script
    - Remove unused preset-adguard-core.sh and preset-clash-core.sh
    - Set default LAN/DHCP/IPv6/firewall settings in init-settings.sh
    - Drop 'by Haiibo' version branding

--- diy-script.sh
+++ diy-script.sh
@@ -34,11 +34,7 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # Themes
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
-# 更改 Argon 主题背景
-# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
 
 # 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
@@ -55,7 +51,7 @@ sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/fil
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/${orig_version}/R${date_version}/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修复 hostapd 报错
 cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
@@ -68,14 +64,6 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
-
-# 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-
-# 调整 V2ray服务器 到 VPN 菜单
-# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
-# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# Set default theme to luci-theme-argon
-uci set luci.main.mediaurlbase='/luci-static/argon'
+# Set default theme to luci-theme-aurora
+uci set luci.main.mediaurlbase='/luci-static/aurora'
 uci commit luci
 
 # Disable IPV6 ula prefix
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+
+# LAN 网络默认配置（静态 IP / 网关 / DNS，关闭 DHCP 与 IPv6 服务）
+uci -q delete network.lan.dns
+uci set network.lan.netmask='255.255.255.0'
+uci set network.lan.gateway='192.168.0.1'
+uci add_list network.lan.dns='192.168.0.1'
+uci -q delete network.lan.ip6assign
+uci set network.lan.delegate='0'
+uci set dhcp.lan.ignore='1'
+uci set dhcp.lan.ra='disabled'
+uci set dhcp.lan.dhcpv6='disabled'
+uci set dhcp.lan.ndp='disabled'
+uci commit network
+uci commit dhcp
+
+# 防火墙默认配置：关闭 SYN-flood 防御，lan 区域开启 IP 动态伪装
+uci set firewall.@defaults[0].synflood_protect='0'
+uci set firewall.@zone[0].masq='1'
+uci commit firewall
 
 # Check file system during boot
 # uci set fstab.@global[0].check_fs=1
