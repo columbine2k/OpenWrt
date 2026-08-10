ebe3deb9406315e1563613e1eb8e81b0b2d1fae3
Soh <columbine.k@gmail.com>
2026-08-10 08:52:27 +0800

    build: disable custom argon background, drop bg1.jpg
    The bg1 background is no longer applied; use the theme's default.

--- diy-mini.sh
+++ diy-mini.sh
@@ -35,7 +35,7 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon packa
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+# cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
