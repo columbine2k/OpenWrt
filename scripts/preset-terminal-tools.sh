2c44eb6ad3f3b13a0f3951e86e793bc2d2052ee7
Soh <columbine.k@gmail.com>
2026-08-10 23:20:29 +0800

    feat: bake vim config, plugins and colorschemes into firmware
    - Ship scripts/vimrc as system-wide /usr/share/vim/vimrc
    - Install vim-tmux-navigator and lightline into /usr/share/vim/vim82
    - Add material and one colorschemes to /usr/share/vim/vim82/colors

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -15,3 +15,26 @@ git clone --depth=1 https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/cu
 cp $GITHUB_WORKSPACE/scripts/.zshrc .
 
 popd
+
+# Get vimrc as system-wide vim config
+mkdir -p files/usr/share/vim
+cp $GITHUB_WORKSPACE/scripts/vimrc files/usr/share/vim/vimrc
+
+# Install vim plugins (vim-tmux-navigator, lightline)
+mkdir -p files/usr/share/vim/vim82/plugin files/usr/share/vim/vim82/autoload files/usr/share/vim/vim82/doc
+git clone --depth=1 https://github.com/christoomey/vim-tmux-navigator /tmp/vim-tmux-navigator
+git clone --depth=1 https://github.com/itchyny/lightline.vim /tmp/lightline.vim
+cp /tmp/vim-tmux-navigator/plugin/tmux_navigator.vim files/usr/share/vim/vim82/plugin/
+cp /tmp/vim-tmux-navigator/doc/tmux-navigator.txt files/usr/share/vim/vim82/doc/
+cp /tmp/lightline.vim/plugin/lightline.vim files/usr/share/vim/vim82/plugin/
+cp -r /tmp/lightline.vim/autoload/lightline.vim /tmp/lightline.vim/autoload/lightline files/usr/share/vim/vim82/autoload/
+cp /tmp/lightline.vim/doc/lightline.txt files/usr/share/vim/vim82/doc/
+rm -rf /tmp/vim-tmux-navigator /tmp/lightline.vim
+
+# Install vim colorschemes (material, one)
+mkdir -p files/usr/share/vim/vim82/colors
+git clone --depth=1 https://github.com/kaicataldo/material.vim /tmp/material.vim
+git clone --depth=1 https://github.com/rakr/vim-one /tmp/vim-one
+cp /tmp/material.vim/colors/material.vim files/usr/share/vim/vim82/colors/
+cp /tmp/vim-one/colors/one.vim files/usr/share/vim/vim82/colors/
+rm -rf /tmp/material.vim /tmp/vim-one
